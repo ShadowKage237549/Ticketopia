8af1450d38321f5b951cf4ded2f7de7d3e8bffdd
Courie Gomez <courie99@ymail.com>
2018-10-16 16:47:20 -0500

    Skeleton Components

--- Project2/Angular/src/app/app.module.ts
+++ Project2/Angular/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ForumComponent } from './Components/forum/forum.component';
+import { StoreComponent } from './Components/store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ForumComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule
