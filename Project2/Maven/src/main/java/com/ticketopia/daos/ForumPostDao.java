e0d593d4ebadcd1aaaebc9b0fe9642f462dce952
Ben Slappey <slappeyb@gmail.com>
2018-10-18 10:28:31 -0500

    created daos and fixed folder hierarchy

---
+++ Project2/Maven/src/main/java/com/ticketopia/daos/ForumPostDao.java
@@ -0,0 +1,11 @@
+package com.ticketopia.daos;
+
+public interface ForumPostDao {
+	// create post
+			// takes in topic
+			// adds post to database
+				// include user who made it and timestamp
+		
+		// get all posts
+			// returns a list of all the posts
+}
