31cf9ae5bb498ca1bd0b9bac2cbd7152b1b0c020
Cory.Broadwell <corbroadwell@gmail.com>
2018-10-19 14:03:15 -0500

    again...

--- Project2/Maven/src/main/webapp/runtime.js
+++ Project2/Maven/src/main/webapp/runtime.js
@@ -136,7 +136,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://18.222.219.232:8085/Ticketopia-0.0.1/";
+/******/ 	__webpack_require__.p = "http://18.222.219.232:8085/Ticketopia/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
