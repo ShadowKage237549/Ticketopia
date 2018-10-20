178eba289622bd8ecc2d4793f6bb6678cb9921e3
Ben Slappey <slappeyb@gmail.com>
2018-10-20 16:34:01 -0500

    implemented interface

--- Project2/Maven/src/main/java/com/ticketopia/daos/CustomerInfoDaoImpl.java
+++ Project2/Maven/src/main/java/com/ticketopia/daos/CustomerInfoDaoImpl.java
@@ -5,9 +5,10 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.ticketopia.beans.CustomerInfo;
+import com.ticketopia.beans.UserRoles;
 import com.ticketopia.util.HibernateUtil;
 
-public class CustomerInfoDaoImpl {
+public class CustomerInfoDaoImpl implements CustomerInfoDao {
 	public boolean createCustomer(CustomerInfo customer) {
 		Session session = HibernateUtil.getSession();
 		Transaction tx = null;
@@ -24,5 +25,32 @@ public class CustomerInfoDaoImpl {
 		}finally {
 			session.close();
 		}
+	}
+	
+	public boolean createPaymentMethod(String email, 
+			Integer cardNumber,
+			Integer securityCode,
+			String expirationDate,
+			String billingAddress,
+			String billingCity,
+			String billingState,
+			Integer billingZip) {
+		return false;
+	}
+	
+	public boolean adjustUserRole(String email, UserRoles role) {
+		return false;
+	}
+	
+	public boolean changePassword(String email, String oldPassword) {
+		return false;
+	}
+	
+	public boolean removePaymentMethod(String email, UserRoles role) {
+		return false;
+	}
+	
+	public void applyPoints(Double points) {
+		
 	}
 }
