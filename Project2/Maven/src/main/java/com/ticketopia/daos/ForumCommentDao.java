111ee2315b70775f61eef1a3b4a2ad1ad8a79424
Philip Prososki <prososkip@outlook.com>
2018-10-20 16:13:49 -0500

    Comments DAO updated.

--- Project2/Maven/src/main/java/com/ticketopia/daos/ForumCommentDao.java
+++ Project2/Maven/src/main/java/com/ticketopia/daos/ForumCommentDao.java
@@ -1,5 +1,10 @@
 package com.ticketopia.daos;
 
+import java.util.List;
+
+import com.ticketopia.beans.Comments;
+import com.ticketopia.beans.ForumPosts;
+
 public interface ForumCommentDao {
 	// create new comment
 			// takes in topic and post
@@ -9,4 +14,15 @@ public interface ForumCommentDao {
 		// get comments
 			// is sent a topic and a post
 			// returns a list of comments under those categories
+	
+	public List <Comments> getPostComments(ForumPosts post);
+	
+	public boolean NewComment(
+			
+			int postId,
+			String displayName,
+			String timeStamp,
+			String commentContent
+			);
+	
 }
