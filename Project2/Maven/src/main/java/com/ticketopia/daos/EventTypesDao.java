0249f67814d05f70f1c733e3db7a6590a01d3405
Courie Gomez <courie99@ymail.com>
2018-10-21 11:51:55 -0500

    Update EventType

--- Project2/Maven/src/main/java/com/ticketopia/daos/EventTypesDao.java
+++ Project2/Maven/src/main/java/com/ticketopia/daos/EventTypesDao.java
@@ -6,6 +6,8 @@ public interface EventTypesDao {
 	
 	//Create new eventType
 	public boolean createEventType(EventType eventType);
+	//Update an existing EventType
+	public boolean UpdateEventType(EventType eventType);
 	//Return an array of eventTypes
 	public EventType[] selectAllEventTypes();
 	//Return a single EventType by its ID
