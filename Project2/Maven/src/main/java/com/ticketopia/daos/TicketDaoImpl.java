fade79cfda4a6e03db8284bd7c89550153736ff1
Ben Slappey <slappeyb@gmail.com>
2018-10-21 10:29:11 -0500

    first commit

---
+++ Project2/Maven/src/main/java/com/ticketopia/daos/TicketDaoImpl.java
@@ -0,0 +1,11 @@
+package com.ticketopia.daos;
+
+import java.util.List;
+
+import com.ticketopia.beans.Ticket;
+
+public class TicketDaoImpl implements TicketDao {
+	public List<Ticket> getAllTickets() {
+		
+	}
+}
