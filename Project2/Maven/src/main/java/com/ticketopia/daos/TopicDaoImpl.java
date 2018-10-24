31307777bf9f645ff702af844e2e413054894adb
Cory.Broadwell <corbroadwell@gmail.com>
2018-10-24 13:24:34 -0500

    add Topic Daos

---
+++ Project2/Maven/src/main/java/com/ticketopia/daos/TopicDaoImpl.java
@@ -0,0 +1,21 @@
+package com.ticketopia.daos;
+
+import java.util.List;
+
+import com.ticketopia.beans.Topic;
+
+public class TopicDaoImpl implements TopicDao{
+
+	@Override
+	public List<Topic> getAllTopics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addTopic(Topic T) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
