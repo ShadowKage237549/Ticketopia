48cf3f8304ca4504be77c3b88b7d257f3f5474c3
Cory.Broadwell <corbroadwell@gmail.com>
2018-10-24 15:16:13 -0500

    push

--- Project2/Maven/src/main/java/com/ticketopia/util/HibernateUtil.java
+++ Project2/Maven/src/main/java/com/ticketopia/util/HibernateUtil.java
@@ -2,6 +2,7 @@ package com.ticketopia.util;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
