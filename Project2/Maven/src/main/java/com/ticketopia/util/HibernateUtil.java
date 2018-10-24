20820ab5359aecb2aea18efe80936058545057a9
Cory.Broadwell <corbroadwell@gmail.com>
2018-10-24 14:13:58 -0500

    push

--- Project2/Maven/src/main/java/com/ticketopia/util/HibernateUtil.java
+++ Project2/Maven/src/main/java/com/ticketopia/util/HibernateUtil.java
@@ -5,12 +5,19 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+	
 	private static SessionFactory sessionFactory =
 			new Configuration()
 			.configure()
 			.buildSessionFactory();
 	
 	public static Session getSession(){
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return sessionFactory.openSession();
 	}
 }
