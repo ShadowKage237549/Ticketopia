f92e1d96dee0dc50e66db22a2d9aa4562c6cc693
Courie Gomez <courie99@ymail.com>
2018-10-25 20:35:45 -0500

    added payment info

---
+++ Project2/Angular/src/app/Components/paymentinfo/paymentinfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentinfo',
+  templateUrl: './paymentinfo.component.html',
+  styleUrls: ['./paymentinfo.component.css']
+})
+export class PaymentinfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
