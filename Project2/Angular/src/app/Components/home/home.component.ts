a7cf55083ccef370eba0459ec606e7446d476afb
Cory.Broadwell <corbroadwell@gmail.com>
2018-10-25 21:06:53 -0500

    push

--- Project2/Angular/src/app/Components/home/home.component.ts
+++ Project2/Angular/src/app/Components/home/home.component.ts
@@ -4,20 +4,20 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  partners:Partner[];
-  constructor(private homeService:HomeService) { }
+    partners: Partner[];
+    constructor(private homeService: HomeService) { }
 
-  ngOnInit() {
-    this.subscribeToPartners();
-  }
-  
-  subscribeToPartners(){
-    this.homeService.getPartners().subscribe((data: Partner[]) => this.partners = data);
-  }
+    ngOnInit() {
+        // this.subscribeToPartners();
+    }
+
+    subscribeToPartners() {
+        this.homeService.getPartners().subscribe((data: Partner[]) => this.partners = data);
+    }
 
 }
