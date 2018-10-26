5a6fc19406b5fdd1f8456ca50658f7c9f013b356
Cory.Broadwell <corbroadwell@gmail.com>
2018-10-26 17:11:18 -0500

    asdf

--- Project2/Maven/src/main/java/com/ticketopia/servlets/CustomerServlet.java
+++ Project2/Maven/src/main/java/com/ticketopia/servlets/CustomerServlet.java
@@ -1,10 +1,15 @@
 package com.ticketopia.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ticketopia.beans.CustomerInfo;
 
 /**
  * Servlet implementation class CustomerServlet
@@ -19,13 +24,17 @@ public class CustomerServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ObjectMapper om = new ObjectMapper();
+		CustomerInfo ci = om.convertValue(request.getParameter("token"), CustomerInfo.class);
+		ci.setPassword("");
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.print(om.writeValueAsString(ci));
 	}
 
 	/**
