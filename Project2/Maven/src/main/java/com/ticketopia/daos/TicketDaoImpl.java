d7100ca898f9a60aad534d68faa2ce9ae5372670
Ben Slappey <slappeyb@gmail.com>
2018-10-27 12:57:08 -0500

    fixed ticket update

--- Project2/Maven/src/main/java/com/ticketopia/daos/TicketDaoImpl.java
+++ Project2/Maven/src/main/java/com/ticketopia/daos/TicketDaoImpl.java
@@ -71,7 +71,7 @@ public class TicketDaoImpl implements TicketDao {
 	}
 	
 	@Override
-	public Boolean updateTicketType(Ticket ticket, String type) {
+	public boolean updateTicket(Ticket ticket) {
 		Session session = null;
 		Transaction tx = null;
 		
@@ -79,143 +79,7 @@ public class TicketDaoImpl implements TicketDao {
 			session = HibernateUtil.getSession();
 			tx = session.beginTransaction();
 			Ticket t = (Ticket) session.get(Ticket.class, ticket.getTicketId());
-			t.setTicketType(type);
-			session.update(t);
-			tx.commit();
-			return true;
-		} catch(HibernateException e) {
-			e.printStackTrace();
-			tx.rollback();
-		} finally {
-			session.close();
-		}
-		return false;
-	}
-	
-	@Override
-	public Boolean updateTicketEventType(Ticket ticket, EventType eventType) {
-		Session session = null;
-		Transaction tx = null;
-		
-		try { 
-			session = HibernateUtil.getSession();
-			tx = session.beginTransaction();
-			Ticket t = (Ticket) session.get(Ticket.class, ticket.getTicketId());
-			t.setEventType(eventType);
-			session.update(t);
-			tx.commit();
-			return true;
-		} catch(HibernateException e) {
-			e.printStackTrace();
-			tx.rollback();
-		} finally {
-			session.close();
-		}
-		return false;
-	}
-	
-	@Override
-	public Boolean updateTicketPrice(Ticket ticket, Double price) {
-		Session session = null;
-		Transaction tx = null;
-		
-		try { 
-			session = HibernateUtil.getSession();
-			tx = session.beginTransaction();
-			Ticket t = (Ticket) session.get(Ticket.class, ticket.getTicketId());
-			t.setTicketPrice(price);
-			session.update(t);
-			tx.commit();
-			return true;
-		} catch(HibernateException e) {
-			e.printStackTrace();
-			tx.rollback();
-		} finally {
-			session.close();
-		}
-		return false;
-	}
-	
-	@Override
-	public Boolean updateTicketDescription(Ticket ticket, String description) {
-		Session session = null;
-		Transaction tx = null;
-		
-		try { 
-			session = HibernateUtil.getSession();
-			tx = session.beginTransaction();
-			Ticket t = (Ticket) session.get(Ticket.class, ticket.getTicketId());
-			t.setEventDescription(description);
-			session.update(t);
-			tx.commit();
-			return true;
-		} catch(HibernateException e) {
-			e.printStackTrace();
-			tx.rollback();
-		} finally {
-			session.close();
-		}
-		return false;
-	}
-	
-	@Override
-	public Boolean updateTicketAddress(Ticket ticket, String address,
-											String city, String state, Integer zip) {
-		Session session = null;
-		Transaction tx = null;
-		
-		try { 
-			session = HibernateUtil.getSession();
-			tx = session.beginTransaction();
-			Ticket t = (Ticket) session.get(Ticket.class, ticket.getTicketId());
-			t.setEventAddress(address);
-			t.setEventCity(city);
-			t.setEventState(state);
-			t.setEventZip(zip);
-			session.update(t);
-			tx.commit();
-			return true;
-		} catch(HibernateException e) {
-			e.printStackTrace();
-			tx.rollback();
-		} finally {
-			session.close();
-		}
-		return false;
-	}
-	
-	@Override
-	public Boolean updateTicketSeat(Ticket ticket, String seat) {
-		Session session = null;
-		Transaction tx = null;
-		
-		try { 
-			session = HibernateUtil.getSession();
-			tx = session.beginTransaction();
-			Ticket t = (Ticket) session.get(Ticket.class, ticket.getTicketId());
-			t.setSeat(seat);
-			session.update(t);
-			tx.commit();
-			return true;
-		} catch(HibernateException e) {
-			e.printStackTrace();
-			tx.rollback();
-		} finally {
-			session.close();
-		}
-		return false;
-	}
-	
-	@Override
-	public Boolean updateTicketFreeStatus(Ticket ticket, Boolean flag) {
-		Session session = null;
-		Transaction tx = null;
-		
-		try { 
-			session = HibernateUtil.getSession();
-			tx = session.beginTransaction();
-			Ticket t = (Ticket) session.get(Ticket.class, ticket.getTicketId());
-			t.setFree(flag);
+			t = ticket;
 			session.update(t);
 			tx.commit();
 			return true;
