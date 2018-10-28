efd6ac158a769550d97df3d55cf5aac4088a125e
Cory.Broadwell <corbroadwell@gmail.com>
2018-10-27 20:43:46 -0500

    push

--- Project2/Angular/src/app/Components/ticket/ticket.component.ts
+++ Project2/Angular/src/app/Components/ticket/ticket.component.ts
@@ -3,7 +3,7 @@ import { Ticket } from './../store/ticket/ticket';
 import { Component, OnInit } from '@angular/core';
 import { TopicService } from '../../Services/Forumtopic/topic.service';
 import { Topic } from '../forumtopic/topic/topic';
-import { ForumPost } from '../forumpost/post/ForumPost';
+import { PostTitle } from '../forumpost/post/ForumPost';
 import { ForumpostService } from '../../Services/ForumPost/forumpost.service';
 async function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -15,15 +15,25 @@ async function delay(ms: number) {
 })
 export class TicketComponent implements OnInit {
     ticket: Ticket;
+    ticketId: number;
     topic: Topic;
-    forumposts: ForumPost[];
+    postTitles: PostTitle[];
     constructor(private ticketService: TicketService, private topicService: TopicService, private fps: ForumpostService) { }
 
     ngOnInit() {
-        this.ticket = this.ticketService.ticket;
-        (async () => { })()
-        this.topic = this.topicService.getTopicById(this.ticket.ticketId);
-        this.forumposts = this.fps.getPostsById(this.topic.topicId);
+
+        (async () => {
+            this.ticket = this.ticketService.ticket;
+            console.log(this.ticket);
+            this.ticketId = Number.parseInt(localStorage.getItem("ticketId"), 10);
+            console.log(this.ticketId)
+            this.topic = this.topicService.getTopicById(this.ticketId);
+
+            await delay(1500); console.log(this.topic);
+            this.postTitles = this.fps.getPostsById(this.ticketId);
+            console.log(this.postTitles);
+            await delay(1500);
+        })();
     }
 
 }
--- Project2/Angular/src/app/Services/ForumPost/forumpost.service.ts
+++ Project2/Angular/src/app/Services/ForumPost/forumpost.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthenticationService } from '../Authentication/authentication.service';
-import { ForumPost } from 'src/app/Components/forumpost/post/ForumPost';
+import { PostTitle } from 'src/app/Components/forumpost/post/ForumPost';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ForumpostService {
-    forumposts: ForumPost[];
+    postTitles: PostTitle[];
     constructor(private http: HttpClient, private auth: AuthenticationService) { }
 
     getPostsById(id: number) {
-        this.http.get(this.auth.url + "postTitle.do?id=" + id).subscribe((data: ForumPost[]) => this.forumposts = data);
-        return this.forumposts;
+        this.http.get(this.auth.url + "postTitle.do?id=" + id).subscribe((data: PostTitle[]) => this.postTitles = data);
+        return this.postTitles;
     }
 }
