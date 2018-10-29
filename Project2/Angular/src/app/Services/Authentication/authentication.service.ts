58d5ab552e1a596d3a3392cc28bd84894be5a2e7
revcg <44207123+revcg@users.noreply.github.com>
2018-10-29 08:59:43 -0500

    Update authentication.service.ts

--- Project2/Angular/src/app/Services/Authentication/authentication.service.ts
+++ Project2/Angular/src/app/Services/Authentication/authentication.service.ts
@@ -13,7 +13,7 @@ async function delay(ms: number) {
 
 export class AuthenticationService {
 
-    public url = 'http://18.222.219.232:8085/Ticketopia/';
+    public url = 'http://18.218.104.150:8085/Ticketopia/';
     // public url = 'http://localhost:8085/Ticketopia/';
     customerinfo: CustomerInfo = null;
     constructor(private http: HttpClient, private router: Router) { }
